723414a04d5f5c287428d972879f18eac1551e90
san1rope <valetinles@gmail.com>
2023-09-24 10:40:27 +0300

    Update

--- tg_bot/handlers/rp_commands.py
+++ tg_bot/handlers/rp_commands.py
@@ -5,6 +5,8 @@ from aiogram import Router, types, F, enums
 from aiogram.filters import Command
 from aiogram.utils.markdown import hcode
 
+from tg_bot.config import Config
+
 logger = logging.getLogger(__name__)
 router = Router()
 
@@ -13,11 +15,16 @@ router = Router()
 async def cmd_me(message: types.Message):
     logger.info(f"Handler called. {cmd_me.__name__}. user_id={message.from_user.id}")
 
-    await message.delete()
+    if Config.BOT.id not in [admin.user.id for admin in await message.chat.get_administrators()]:
+        return
 
     args = message.text.strip().split()
+    if not args:
+        return
+
     args[1] = args[1].lower()
 
+    await message.delete()
     await message.answer(
         text=f"<b>{hcode(message.from_user.full_name)} </b>{' '.join(args[1:])}",
         disable_web_page_preview=True
@@ -28,13 +35,19 @@ async def cmd_me(message: types.Message):
 async def cmd_do(message: types.Message):
     logger.info(f"Handler called. {cmd_do.__name__}. user_id={message.from_user.id}")
 
-    await message.delete()
+    if Config.BOT.id not in [admin.user.id for admin in await message.chat.get_administrators()]:
+        return
+
+    args = message.text.strip().split()
+    if not args:
+        return
 
     args = message.text.strip().split()
     args[1] = args[1][0].upper() + args[1][1:]
     if not args[-1].endswith('.'):
         args[-1] = args[-1] + '.'
 
+    await message.delete()
     await message.answer(
         text=f"{' '.join(args[1:])} | {hcode(message.from_user.full_name)}",
         disable_web_page_preview=True
@@ -45,11 +58,17 @@ async def cmd_do(message: types.Message):
 async def cmd_try(message: types.Message):
     logger.info(f"Handler called. {cmd_try.__name__}. user_id={message.from_user.id}")
 
-    await message.delete()
+    if Config.BOT.id not in [admin.user.id for admin in await message.chat.get_administrators()]:
+        return
+
+    args = message.text.strip().split()
+    if not args:
+        return
 
     args = message.text.strip().split()
     args[1] = args[1].lower()
 
+    await message.delete()
     await message.answer(
         text=f"{hcode(message.from_user.full_name)} {' '.join(args[1:])} | <b>{'Вдало' if randint(0, 2) else 'Невдало'}</b>",
         disable_web_page_preview=True
@@ -60,10 +79,14 @@ async def cmd_try(message: types.Message):
 async def global_chat(message: types.Message):
     logger.info(f"Handler called. {global_chat.__name__}. user_id={message.from_user.id}")
 
-    await message.delete()
+    if Config.BOT.id not in [admin.user.id for admin in await message.chat.get_administrators()]:
+        return
 
     args = message.text.strip().split()
+    if not args:
+        return
 
+    await message.delete()
     await message.answer(
         text=f"{hcode(message.from_user.full_name)} говорить: {' '.join(args)}",
         disable_web_page_preview=True
